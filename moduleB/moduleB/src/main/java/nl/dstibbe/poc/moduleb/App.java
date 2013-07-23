8119675bec587194f13df4c34ba16fd9e0830789
David Stibbe <dstibbe@gmail.com>
2013-07-23 21:53:40 +0200

    First setup module B

---
+++ moduleB/moduleB/src/main/java/nl/dstibbe/poc/moduleb/App.java
@@ -0,0 +1,16 @@
+package nl.dstibbe.poc.moduleb;
+import nl.dstibbe.poc.modulea.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Module B says:" );
+        nl.dstibbe.poc.modulea.App.main(args);
+        
+    }
+}
