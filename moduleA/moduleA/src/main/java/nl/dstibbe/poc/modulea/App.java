044bd1fd67e0713bd07c3e0a91cff4094133f1b9
David Stibbe <dstibbe@gmail.com>
2013-07-23 21:52:10 +0200

    First setup module A

---
+++ moduleA/moduleA/src/main/java/nl/dstibbe/poc/modulea/App.java
@@ -0,0 +1,13 @@
+package nl.dstibbe.poc.modulea;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! - v1.0-SNAPSHOT" );
+    }
+}
