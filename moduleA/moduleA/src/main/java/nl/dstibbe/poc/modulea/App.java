9c277af17bc977e2e920fcd4590fbf4fa780ecd2
David Stibbe <dstibbe@gmail.com>
2013-07-23 22:13:20 +0200

    Use build number instead of "SNAPSHOT"

--- moduleA/moduleA/src/main/java/nl/dstibbe/poc/modulea/App.java
+++ moduleA/moduleA/src/main/java/nl/dstibbe/poc/modulea/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! - v1.0-SNAPSHOT" );
+        System.out.println( "Hello World! - v1.0-{build.number}" );
     }
 }
